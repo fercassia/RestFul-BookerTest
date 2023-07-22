7d3db6e8c35b598df0314b411f3b0d8bed3dee4b
fercassia <feh.cmrg@outlook.com>
2023-07-22 15:19:25 -0300

    test: creation of test about creating booking - POST

--- src/test/java/com/javabook/bases/BaseSetup.java
+++ src/test/java/com/javabook/bases/BaseSetup.java
@@ -1,6 +1,4 @@
 package com.javabook.bases;
-
-import com.javabook.GlobalParameters;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
@@ -11,18 +9,21 @@ import org.testng.annotations.BeforeClass;
 
 import static org.hamcrest.number.OrderingComparison.lessThan;
 
-public abstract class BaseStup {
+public abstract class BaseSetup {
 
     private String _contetType = "Content-Type";
 
     private String _accept = "Accept";
 
+    private String _requestResponseModel = "application/json";
+    private String _basePath = "https://restful-booker.herokuapp.com";
+
     @BeforeClass
     public void setup(){
-        RequestSpecification requestSpecification =new RequestSpecBuilder()
-                .setBaseUri(GlobalParameters.URI_DEFAUL)
-                .addHeader(_contetType,  GlobalParameters.REQUEST_RESPONSE_MODEL)
-                .addHeader(_accept,GlobalParameters.REQUEST_RESPONSE_MODEL)
+        RequestSpecification requestSpecification = new RequestSpecBuilder()
+                .setBaseUri(_basePath)
+                .addHeader(_contetType, _requestResponseModel)
+                .addHeader(_accept, _requestResponseModel)
                 .addFilter(new RequestLoggingFilter())//Will log request before it's passed to http builder
                 .addFilter(new RequestLoggingFilter())//Will print the response body if response matches a given status cde
                 .build();
