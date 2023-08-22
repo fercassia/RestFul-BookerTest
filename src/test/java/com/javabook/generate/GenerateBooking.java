7862b75cfd370267474e44d9fea879393e0b0c56
fercassia <feh.cmrg@outlook.com>
2023-08-21 21:13:32 -0300

    refactor: atualização pom

---
+++ src/test/java/com/javabook/generate/GenerateBooking.java
@@ -0,0 +1,2 @@
+package com.javabook.generate;public class GenerateBooking {
+}
---
+++ src/test/java/com/javabook/objects/PartialBookingWithoutAdditionalNeedsObject.java
@@ -0,0 +1,9 @@
+package com.javabook.objects;
+
+public class BookingWithoutAdditionalNeedsObject {
+    private String firstname;
+    private String lastname;
+    private int totalprice;
+    private boolean depositpaid;
+    private BookingDatesObject bookingdates;
+}
